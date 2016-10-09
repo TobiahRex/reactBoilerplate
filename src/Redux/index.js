07ada6fe4017d99238a8b42c73744cba7c8262ed
TobiahRex <tobiah.rex@gmail.com>
2016-10-08 23:23:35 -0700

    created root reducer inside Redux as index.js

---
+++ src/Redux/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+import configureStore from '../Store/configureStore';
+import rootSaga from '../Sagas/';
+import { thingReducer } from '../Redux/ThingRedux';
+
+// const appReducer = combineReducers({
+//   things,
+// });
+//
+// const rootReducer = (state, action) => appReducer(state, action);
+//
+//
+// export default rootReducer;
+
+export default () => {
+  const things = thingReducer;
+  const rootReducer = combineReducers({
+    things,
+  });
+  return configureStore(rootReducer, rootSaga);
+};
