37274f2f166734a3f0c2b8a799b438327648f2e1
TobiahRex <tobiah.rex@gmail.com>
2016-10-08 23:20:29 -0700

    refactored rootReducer declaration for one function.

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,11 +1,20 @@
 import { combineReducers } from 'redux';
+import configureStore from '../Store/configureStore';
+import rootSaga from '../Sagas/'
 import things from './ThingReducer';
 
-const appReducer = combineReducers({
-  things,
-});
+// const appReducer = combineReducers({
+//   things,
+// });
+//
+// const rootReducer = (state, action) => appReducer(state, action);
+//
+//
+// export default rootReducer;
 
-const rootReducer = (state, action) => appReducer(state, action);
-
-
-export default rootReducer;
+export default () => {
+  const rootReducer = combineReducers({
+    things,
+  });
+  return configureStore(rootReducer, rootSaga);
+}
