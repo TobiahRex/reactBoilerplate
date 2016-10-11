e7e3f28ace1f3ae2a5721693e05b02f4acec1315
TobiahRex <tobiah.rex@gmail.com>
2016-10-11 12:06:07 -0700

    renamed deleteThing to removeThing @ Containers/Things.js

---
+++ src/Components/ThingList.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import uuid from 'uuid';
+
+const ThingList = ({ removeThing, editThing, things }) => {
+
+  const eachThing = things.map((thing) => (
+      <div key={uuid()}>
+        <Thing data={thing} editThing={editThing} removeThing={removeThing} />
+      </div>
+    )
+  );
+
+  return (
+    <div>
+      {eachThing}
+    </div>
+  )
+}
+
+ThingList.propTypes = {
+  removeThing: PropTypes.func.isRequired,
+  editThing: PropTypes.func.isRequired,
+  things: PropTypes.array.isRequired, // eslint-disable-line
+}
+
+export default ThingList;
