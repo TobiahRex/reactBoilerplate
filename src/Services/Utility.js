08509f24bcbd532c7142934379661a3144ae906b
TobiahRex <tobiah.rex@gmail.com>
2016-10-11 16:19:42 -0700

    full crud TESTED SAT on react Template. Implementing Material UI now.

--- src/Services/Utility.js
+++ src/Services/Utility.js
@@ -4,7 +4,6 @@ export default (response) => {
   if (response.problem) {
     toastr.error(response.problem, 'API Error');
     throw new Error('API Error: ', response.problem);
-    return response.problem
   } else {
     toastr.success('API Success!');
     return response.data;
