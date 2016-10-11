e76d65241d001c71c067552b67baddc685e7c4aa
TobiahRex <tobiah.rex@gmail.com>
2016-10-11 13:34:33 -0700

    Thing component rendering correctly.js

--- src/Components/Thing.js
+++ src/Components/Thing.js
@@ -6,57 +6,69 @@ export default class Thing extends Component {
 
     this.state = {
       data: this.props.data,
-      newData: this.props.data,
+      newName: this.props.data.name,
       edit: false,
-    }
+    };
 
     this.endableEdit = this.enableEdit.bind(this);
     this.submitEdit = this.submitEdit.bind(this);
     this.cancelEdit = this.cancelEdit.bind(this);
+    this.removeThing = this.removeThing.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({ newName: e.target.value });
+  }
+
+  cancelEdit() {
+    this.setState({
+      edit: false,
+      data: this.props.data,
+    });
+  }
+
+  submitEdit() {
+    const { newThing } = this.state.data;
+    newThing.name = this.state.newName;
+
+    this.props.editThing(newThing);
+    this.setState({ newName: '', data: {} });
   }
 
   enableEdit() {
     this.setState({ edit: true });
   }
 
-  submitEdit() {
-    this.props.editThing(this.state.newData);
-    this.setState({ newData: '', data: '' });
+  removeThing(id) {
+    this.props.removeThing(id);
   }
 
-  cancelEdit() {
-    this.setState({
-      edit: false,
-      data: this.props.data
-    });
-  }
-
-  onInputChange(e) {
-    this.setState({ newData: e.target.value });
-  }
-
-  // this.props = data, editThing, removeThing
-  return (
-    <div>
-      {
-        this.state.edit
-        ?
-        <input onChange={(e) => onInputChange(e)} value={this.state.newData}/>
+  render() {
+    console.log('this.props.data: ', this.props.data);
+    // this.props = data, editThing, removeThing
+    const buttonSet = this.state.edit ?
+      <div>
+        <input onChange={e => this.onInputChange(e)} value={this.state.newName} />
         <button submitThing={this.submitEdit} type="submit">Submit</button>
         <button cancelEdit={this.cancelEdit} type="submit">Cancel</button>
-        :
-        <span>{this.state.data}</span>
+      </div>
+    :
+      <div>
+        <span>{this.state.data.name}: </span>
         <button onClick={this.enableEdit} type="buttom">Edit</button>
-        <button removeThing={removeThing(data.id)} type="buttom">Remove</button>
-      }
-    </div>
-  )
+        <button onClick={() => this.removeThing(this.props.data.id)} type="buttom">Remove</button>
+      </div>;
+
+    return (
+      <span>
+        {buttonSet}
+      </span>
+    );
+  }
 }
 
 Thing.propTypes = {
   data: PropTypes.object, //eslint-disable-line
   editThing: PropTypes.func.isRequired,
   removeThing: PropTypes.func.isRequired,
-}
-
-export default Thing;
+};
--- src/Components/ThingList.js
+++ src/Components/ThingList.js
@@ -1,13 +1,12 @@
 import React, { PropTypes } from 'react';
-import Thing from './Thing';
 import uuid from 'uuid';
+import Thing from './Thing';
 
 const ThingList = ({ removeThing, editThing, things }) => {
-
-  const eachThing = things.map((thing) => (
-      <div key={uuid()}>
-        <Thing data={thing} editThing={editThing} removeThing={removeThing} />
-      </div>
+  const eachThing = things.map(thing => (
+    <div key={uuid()}>
+      <Thing data={thing} editThing={editThing} removeThing={removeThing} />
+    </div>
     )
   );
 
@@ -15,13 +14,13 @@ const ThingList = ({ removeThing, editThing, things }) => {
     <div>
       {eachThing}
     </div>
-  )
-}
+  );
+};
 
 ThingList.propTypes = {
   removeThing: PropTypes.func.isRequired,
   editThing: PropTypes.func.isRequired,
   things: PropTypes.array.isRequired, // eslint-disable-line
-}
+};
 
 export default ThingList;
