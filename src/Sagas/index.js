06a8c30bc112f9bbbe115a0d3dc054e006f0b5ff
TobiahRex <tobiah.rex@gmail.com>
2016-10-12 19:36:37 -0700

    removed ApiSagas from rootSaga.js

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -1,9 +1,6 @@
 import { takeLatest } from 'redux-saga';
-// ----- API Sagas ----- //
-import Fetching from './ApiSagas/Fetching';
-import ApiSuccess from './ApiSagas/Success';
-import ApiFail from './ApiSagas/Fail';
-import { ApiTypes } from '../Redux/ApiRedux';
+import { createAPI } from '../Services/API';
+
 // ----- Thing Sagas ----- //
 import GetAllThings from './ThingSagas/GetAllThings';
 import CreateThing from './ThingSagas/CreateThing';
@@ -11,17 +8,10 @@ import EditThing from './ThingSagas/EditThing';
 import RemoveThing from './ThingSagas/RemoveThing';
 import { ThingTypes } from '../Redux/ThingRedux';
 
-
-import API from '../Services/API';
-
-const api = API.createAPI();
-
+const api = createAPI();
 export default function* rootSaga() {
   yield [takeLatest(ThingTypes.GET_ALL_THINGS, GetAllThings, api),
   takeLatest(ThingTypes.CREATE_THING, CreateThing, api),
   takeLatest(ThingTypes.REMOVE_THING, RemoveThing, api),
-  takeLatest(ThingTypes.EDIT_THING, EditThing, api),
-  takeLatest(ApiTypes.FETCHING, Fetching),
-  takeLatest(ApiTypes.API_FAIL, ApiFail),
-  takeLatest(ApiTypes.API_SUCCESS, ApiSuccess)];
+  takeLatest(ThingTypes.EDIT_THING, EditThing, api)];
 }
