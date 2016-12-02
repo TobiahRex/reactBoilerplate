0dbf605e6c9032204f3b7b30bd6460ea62e551d2
TobiahRex <tobiah.rex@gmail.com>
2016-12-02 00:48:01 -0800

    README update.

--- src/Services/Utility.js
+++
@@ -1,6 +0,0 @@
-// import toastr from 'toastr';
-//
-// export const handle = {
-//   success: (response) => apiActions.apiFail(response.problem),
-//   error: (response) => [put(apiActions.apiSuccess()), put(thingActions.createThingSuccess(response.data))],
-// };
