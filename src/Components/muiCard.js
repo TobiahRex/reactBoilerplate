4b8bb0736c1467e83d4fc546417b1dc1196c0421
TobiahRex <tobiah.rex@gmail.com>
2016-12-01 23:55:45 -0800

    added ignore "true" stateless PureComponents to .eslintrc rules.

--- src/Components/muiCard.js
+++ src/Components/muiCard.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, PureComponent } from 'react';
 import {
   Card,
   CardHeader,
@@ -7,24 +7,25 @@ import {
   RaisedButton,
 } from 'material-ui';
 
-const muiCard = ({ thing }) => (
-  <Card>
-    <CardHeader title="Card Template" subtitle="reusable component" />
-    <CardText>
-      Props: { thing || "'empty'" }
-    </CardText>
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </CardText>
-    <CardActions>
-      <RaisedButton label="Button 1" primary />
-      <RaisedButton label="Button 2" secondary />
-    </CardActions>
-  </Card>
-);
-
-muiCard.propTypes = {
-  thing: PropTypes.string,
-};
-
-export default muiCard;
+export default class muiCard extends PureComponent {
+  static propTypes = {
+    thing: PropTypes.string,
+  }
+  render() {
+    return (
+      <Card>
+        <CardHeader title="Card Template" subtitle="reusable component" />
+        <CardText>
+          Props: { this.props.thing || "'empty'" }
+        </CardText>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </CardText>
+        <CardActions>
+          <RaisedButton label="Button 1" primary />
+          <RaisedButton label="Button 2" secondary />
+        </CardActions>
+      </Card>
+    );
+  }
+}
