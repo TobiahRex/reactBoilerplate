fb9db4238b1cb7b8abe7a3cbaf0178f06eac0653
TobiahRex <tobiah.rex@gmail.com>
2016-12-02 19:47:18 -0800

    added babel-polyfill to App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import React, { PropTypes } from 'react';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
